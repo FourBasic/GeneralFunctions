85c09452b77cb6efa0465fc1e8def2469684c67f
FourBasic <79863927+FourBasic@users.noreply.github.com>
2024-09-07 11:23:40 -0400

    added limit

--- GeneralFunctions.cpp
+++ GeneralFunctions.cpp
@@ -1,8 +1,20 @@
 #include "GeneralFunctions.h"
 #include <Arduino.h>
+
 // Returns true of 'a' in range of 'b' +- tolerance
 bool withinRange(int a, int b, int tol) {
  return abs(a-b) < tol;
+}
+
+// Limits a value within a range
+int limit(int mn, int val, int mx) {
+    if (val < mn) {
+        return mn;
+    } else if (val > mx) { 
+        return mx;
+    } else {
+        return val;
+    }
 }
 
 
